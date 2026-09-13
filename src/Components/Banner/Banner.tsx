cb97bb818febcf1708c4df9b71d9d70b0cc9dc78
Farzana Akter Alo <mstfarzana161@gmail.com>
2026-09-13 18:46:44 +0600

    added banner text style

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -9,17 +9,25 @@ const Banner = () => {
           <div>
             <h1 className="text-5xl font-bold">
               Build Your Ideal <br />
-              <span className=""> Development Stack</span>
+              <span className="bg-gradient-to-r from-[#F97316] via-[#EC4899] to-[#7C3AED] bg-clip-text text-transparent">
+                {" "}
+                Development
+              </span>
+              <span className="bg-gradient-to-r from-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent"> Stack</span>
             </h1>
             <p className="py-6">
               Explore frontend, backend, database, and tooling options, <br />{" "}
               compare them side by side, and put together the stack that fits
               your <br /> next project.
             </p>
-           <div className="flex gap-4 ">
-             <button className="text-white w-48 bg-gradient-to-r from-[#F97316] to-[#EC4899] px-4 py-2 rounded-sm font-semibold">Explore Technologies</button>
-            <button className=" border border-base-300 w-48 px-4 py-2 rounded-sm font-semibold">Learn More</button>
-           </div>
+            <div className="flex gap-4 ">
+              <button className="text-white w-48 bg-gradient-to-r from-[#F97316] to-[#EC4899] px-4 py-2 rounded-sm font-semibold">
+                Explore Technologies
+              </button>
+              <button className=" border border-base-300 w-48 px-4 py-2 rounded-sm font-semibold">
+                Learn More
+              </button>
+            </div>
           </div>
         </div>
       </div>
