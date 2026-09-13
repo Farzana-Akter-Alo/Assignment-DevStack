4731db8b5a3dc6b841fe4d79724f7d7a4110a6e7
Farzana Akter Alo <mstfarzana161@gmail.com>
2026-09-13 22:25:19 +0600

    added sticky style to navbar

--- src/Components/Navbar/Nav.tsx
+++ src/Components/Navbar/Nav.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import logo from "../../assets/logo-text.png"
+import logo from "../../assets/logo-text.png";
 
 function Nav() {
   return (
-    <nav className="bg-base-100 shadow-sm ">
+    <nav className="bg-base-100 shadow-sm sticky top-0 z-50">
       <div className="navbar container mx-auto">
         <div className="navbar-start">
           <img src={logo} alt="" />
@@ -11,7 +10,9 @@ function Nav() {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 text-[16px] font-semibold">
             <li>
-              <a href="#" className="text-[#D91B7E]">Home</a>
+              <a href="#" className="text-[#D91B7E]">
+                Home
+              </a>
             </li>
             <li>
               <a href="#">Technologies</a>
@@ -29,9 +30,10 @@ function Nav() {
         </div>
         <div className="navbar-end gap-6">
           <button className="font-bold">Sign In</button>
-          <button className="text-white bg-[#D91B7E] px-4 py-2 rounded-3xl font-semibold">Sign Up</button>
+          <button className="text-white bg-[#D91B7E] px-4 py-2 rounded-3xl font-semibold">
+            Sign Up
+          </button>
         </div>
-        
       </div>
     </nav>
   );
